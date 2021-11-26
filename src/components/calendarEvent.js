4beedeed6e330131f9d0ca3202762821638a8885
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2021-11-26 21:11:50 +0100

    Changed language in react-big-calendar.

--- src/components/calendarEvent.js
+++ src/components/calendarEvent.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Calendar, momentLocalizer } from "react-big-calendar"
 import moment from "moment"
+import "moment/locale/pl"
 import "react-big-calendar/lib/css/react-big-calendar.css"
 
 import styled from "styled-components"
@@ -9,32 +10,88 @@ const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   width: 100%;
   height: 100%;
   padding: 30px 50px 60px 50px;
+`
+
+const WrapperSelected = styled.div`
+  padding: 30px 15px;
+  width: 100%;
+  height: 24px;
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 24px;
 `
 
 const localizer = momentLocalizer(moment)
 const myEventsList = [
   {
     id: 0,
-    title: "All Day Event very long title",
+    title: "Spotkanie autorskie",
     allDay: true,
-    start: new Date(2015, 3, 0),
-    end: new Date(2015, 3, 1),
+    start: new Date(2021, 10, 26),
+    end: new Date(2021, 10, 26),
+  },
+  {
+    id: 1,
+    title: "Spotkanie autorskie 2",
+    allDay: true,
+    start: new Date(2021, 10, 25),
+    end: new Date(2021, 10, 25),
   },
 ]
 
 export const CalendarEvent = () => {
+  const [selected, setSelected] = React.useState()
+
+  const handleSelectSlot = ({ title }) => {   
+    setSelected(title)
+    return
+  }
+
+
+  const resetSelectSlot = () => {
+  setSelected("")
+  return
+}
   return (
     <Wrapper>
-      <Calendar
-        localizer={localizer}
-        events={myEventsList}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-      />
+      <div>
+        <Calendar
+          localizer={localizer}
+          events={myEventsList}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: 500 }}
+          messages={{
+            next: "Dalej",
+            previous: "Wstecz",
+            today: "Dzisiaj",
+            month: "Мiesiąc",
+            week: "Tydzień",
+            day: "Dzien",
+            work_week: "Roboczy tydzień",
+            allDay: "Cały dzień",
+            yesterday: "Wczoraj",
+            tomorrow: "Jutro",
+            noEventsInRange: "Nie dodano wydarzeń",
+            showMore: function showMore(total) {
+              return "+" + total + "więcej"
+            },
+          }}
+          onSelectEvent={handleSelectSlot}
+          onView={resetSelectSlot}
+        />
+      </div>
+      {selected ? (
+        <WrapperSelected>
+          <span style={{ fontWeight: 600 }}>Nazwa wydarzenia:</span> {selected}
+        </WrapperSelected>
+      ) : (
+        <WrapperSelected />
+      )}
     </Wrapper>
   )
 }
