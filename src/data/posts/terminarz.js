e7009ee0e38104d4d8e6e31e4ba76b45aacf33dd
KolodziejczykTomasz <tomaszkolodziejczyk@wp.pl>
2022-08-25 18:44:20 +0200

    Added new post.

---
+++ src/data/posts/terminarz.js
@@ -0,0 +1,23 @@
+export const terminarz = [
+  {
+    id: 0,
+    title: "Warsztaty w ramach Wieczna młodość",
+    start: new Date(2021, 5, 22, 15, 30, 0, 0),
+    end: new Date(2021, 5, 22, 17, 30, 0, 0),
+    desc: "Warsztaty w ramach Wieczna młodość",
+  },
+  {
+    id: 1,
+    title: "Warsztaty mydlarskie",
+    start: new Date(2021, 5, 23, 15, 30, 0, 0),
+    end: new Date(2021, 5, 23, 17, 30, 0, 0),
+    desc: "Warsztaty mydlarskie",
+  },
+  {
+    id: 2,
+    title: "Narodowe czytanie 2021",
+    start: new Date(2021, 8, 13, 15, 30, 0, 0),
+    end: new Date(2021, 8, 13, 17, 30, 0, 0),
+    desc: "Narodowe czytanie 2021",
+  },
+]
